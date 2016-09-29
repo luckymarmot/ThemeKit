24c980549a7187dea1161b4d28c13d19215da828
Nuno Grilo <nfgrilo@gmail.com>
2016-09-29 10:48:22 +0100

    Demo app skeleton

---
+++ Demo/Demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by Nuno Grilo on 29/09/2016.
+//  Copyright © 2016 Paw Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Nuno Grilo on 29/09/2016.
+//  Copyright © 2016 Paw Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

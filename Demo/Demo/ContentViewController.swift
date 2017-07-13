300abac5cca8c706ec169fd894e90be56e201694
Nuno Grilo <nfgrilo@gmail.com>
2017-07-13 10:32:17 +0100

    Fix unthemed clipping view (visible on 10.13)

--- Demo/Demo/ContentViewController.swift
+++ Demo/Demo/ContentViewController.swift
@@ -37,6 +37,12 @@ class ContentViewController: NSViewController, NSTextDelegate {
         paragraphStyle.lineSpacing = 4.4
         contentTextView.defaultParagraphStyle = paragraphStyle
         
+        // Setup our clipping view background color (text view parent)
+        if let clipView = contentTextView.enclosingScrollView?.contentView {
+            clipView.backgroundColor = ThemeColor.contentBackgroundColor
+            clipView.drawsBackground = true
+        }
+        
         // Observe note selection change notifications
         NotificationCenter.default.addObserver(forName: .didChangeNoteSelection, object: nil, queue: nil) { (notification) in
             let obj = notification.object
@@ -48,8 +54,12 @@ class ContentViewController: NSViewController, NSTextDelegate {
         
         // Fix white scrollbar view when in dark theme and scrollbars are set to
         // always be shown on *System Preferences > General*.
-        contentTextView.enclosingScrollView?.backgroundColor = ThemeColor.contentBackgroundColor
-        contentTextView.enclosingScrollView?.wantsLayer = true
+        if let scrollView = contentTextView.enclosingScrollView {
+            scrollView.backgroundColor = ThemeColor.contentBackgroundColor
+            scrollView.wantsLayer = true
+        }
+        
+        // Observe theme changes
         NotificationCenter.default.addObserver(self, selector: #selector(didChangeTheme(_:)), name: .didChangeTheme, object: nil)
     }
     
